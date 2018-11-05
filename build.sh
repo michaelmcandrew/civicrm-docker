9c45189589732ae41a2f3de3f1c6d07287f7ccf4
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-11-05 22:19:05 +0000

    build mysql docker image

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ set -xe
 docker build -f civicrm-base.dockerfile -t civicrm-base .
 docker build -f civicrm-wordpress.dockerfile -t civicrm-wordpress .
 docker build -f civicrm-drupal.dockerfile -t civicrm-drupal .
+docker build -f civicrm-mysql.dockerfile -t civicrm-mysql .
 set +xe
-echo "\nBuild process completed successfully"
+echo "Build process completed successfully"
