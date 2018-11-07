6c155cf00af397401734073bb17fc291bc0de98c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2018-11-07 13:38:25 +0000

    reverse proxy friendly drupal config

--- drupal/settings.php
+++ drupal/settings.php
@@ -30,6 +30,9 @@ $conf['404_fast_paths_exclude'] = '/\/(?:styles)|(?:system\/files)\//';
 $conf['404_fast_paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
 $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
 
+$conf['reverse_proxy'] = TRUE;
+$conf['reverse_proxy_addresses'] = [$_SERVER['REMOTE_ADDR']];
+
 if(file_exists('settings.extra.php')){
     require_once 'settings.extra.php';
 }
