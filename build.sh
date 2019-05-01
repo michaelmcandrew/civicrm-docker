52302ef70b738a57258d15d0b6ce248aca2cc3ee
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2019-05-02 00:15:26 +0100

    readability

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ docker build -t michaelmcandrew/civicrm-base base
 docker build -t michaelmcandrew/civicrm-wordpress wordpress --build-arg CIVICRM_VERSION=$CIVICRM_VERSION
 docker build -t michaelmcandrew/civicrm-drupal drupal --build-arg CIVICRM_VERSION=$CIVICRM_VERSION
 docker build -t michaelmcandrew/civicrm-mysql mysql
+
 set +xe
+
 echo "Build process completed successfully"
