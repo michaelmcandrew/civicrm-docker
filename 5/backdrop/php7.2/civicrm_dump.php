31cce81070b3e72bb8b10dac31c65c04ec0798e8
Michael McAndrew <michael@3sd.io>
2020-07-21 16:59:32 +0100

    Auto-updating generated files

--- 5/backdrop/php7.2/civicrm_dump.php
+++ 5/backdrop/php7.2/civicrm_dump.php
@@ -1,29 +1,33 @@
 <?php
-function civicrm_dump($state)
-{
 
-    $state['start'] = date(DATE_ISO8601);
-    $tarName = "/state/{$state['project']}." . date('Ymd\THis') . ".tar.gz";
+function civicrm_dump($state) {
 
-    $dumpDir = tempnam('/tmp', 'civicrm_dump_');
-    `rm $dumpDir; mkdir $dumpDir`;
+  $state['start'] = date(DATE_ISO8601);
+  $tarName = "/state/{$state['project']}." . date('Ymd\THis') . ".tar.gz";
 
-    foreach ($state['databases'] as $name) {
-        foreach (['USER', 'PASS', 'HOST', 'PORT', 'NAME'] as $var) {
-            $$var = getenv(strtoupper($name) . '_DB_' . $var);
-        }
-        `mysqldump -u $USER -p$PASS -h $HOST -P $PORT $NAME --single-transaction --skip-triggers | sed -E "/^\/\*\![[:digit:]]+ DEFINER/d" > {$dumpDir}/{$name}.sql`;
+  $dumpDir = tempnam('/tmp', 'civicrm_dump_');
+  `mkdir $dumpDir`;
+
+  foreach ($state['databases'] as $name) {
+    foreach (['USER', 'PASS', 'HOST', 'PORT', 'NAME'] as $var) {
+      $envVar = strtoupper($name) . '_DB_' . $var;
+      if (getenv($envVar) === FALSE) {
+        die("Error: environment variable '$envVar' was not found.\n");
+      }
+      $$var = getenv(strtoupper($name) . '_DB_' . $var);
     }
-    foreach ($state['directories'] as $name => $path) {
-        `tar -czf {$dumpDir}/{$name}.tar.gz -C $path .`;
-    }
+    `mysqldump -u $USER -p$PASS -h $HOST -P $PORT $NAME --single-transaction --skip-triggers | sed -E "/^\/\*\![[:digit:]]+ DEFINER/d" > {$dumpDir}/{$name}.sql`;
+  }
+  foreach ($state['directories'] as $name => $path) {
+    `tar -czf {$dumpDir}/{$name}.tar.gz -C $path .`;
+  }
 
-    $state['end'] = date(DATE_ISO8601);
-    file_put_contents("$dumpDir/state.json", json_encode($state, JSON_PRETTY_PRINT));
+  $state['end'] = date(DATE_ISO8601);
+  file_put_contents("$dumpDir/state.json", json_encode($state, JSON_PRETTY_PRINT));
 
-    `tar -czf $tarName -C $dumpDir .`;
+  `tar -czf $tarName -C $dumpDir .`;
 
-    `rm -r $dumpDir`;
+  `rm -r $dumpDir`;
 
-    echo "$tarName\n";
+  echo "$tarName\n";
 }
