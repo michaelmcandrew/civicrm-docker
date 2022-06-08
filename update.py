25b71390563b5eb49f7e412eb8e80ac703c33eaf
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2022-06-08 14:26:28 +0100

    'Auto-updating generated files'

---
+++ update.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+from subprocess import run
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+run(["git", "-C", PROJECT_DIR, "pull"])
+print(["python", PROJECT_DIR + "/generate.py"])
+print(["python", PROJECT_DIR + "/build.py"])
+print(["python", PROJECT_DIR + "/publish.py"])
+run(["git", "-C", PROJECT_DIR, "add", "."])
+run(["git", "-C", PROJECT_DIR, "commit", "-am", "'Auto-updating generated files'"])
+run(["git", "-C", PROJECT_DIR, "push"])
