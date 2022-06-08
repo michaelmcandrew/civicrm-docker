a6a718fc7720acfe9eecd330a880652741d91b9f
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2022-06-08 15:22:02 +0100

    skip backdrop for now

--- variables.py
+++ variables.py
@@ -5,6 +5,9 @@ latest_civicrm_release = os.popen(
     "curl -s https://latest.civicrm.org/stable.php"
 ).read()
 civi_releases = [latest_civicrm_release]
-cms_variants = ["drupal", "wordpress", "backdrop"]
+cms_variants = [
+    "drupal",
+    "wordpress",
+]  # Skip backdrop for now since Dockerfile is broken
 php_releases = ["7.3", "7.4"]
 defaults = {"civi": latest_civicrm_release, "cms": "drupal", "php": "7.3"}
