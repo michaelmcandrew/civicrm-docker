495c375cc0b2c3a99f849afb81560a3e132d7a9f
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2024-10-18 11:07:05 +0200

    pass full path to docker build

--- buildAndPublish.php
+++ buildAndPublish.php
@@ -9,8 +9,9 @@ $commands = [];
 
 foreach ($images as $image) {
   $tags = implode(' ', array_map(fn($t)=> "--tag $imageName:$t", $image->tags));
+  $dir = __DIR__ . $image->dir;
   // TODO: save the planet - do more caching.
-  $commands[] = "docker build --pull --builder $builder --platform $platforms $image->dir $tags --push";
+  $commands[] = "docker build --pull --builder $builder --platform $platforms $dir $tags --push";
 }
 foreach ($commands as $command) {
   `$command`;
